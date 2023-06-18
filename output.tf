156de9de2a935208cbd512ac6dac872d93c9785f
RalphBrynard <ralph@thebrynards.com>
2023-06-17 23:27:57 -0500

    Title: Outputs
    Body:
    
    Co-authored-by: Ralph Brynard <ralph@thebrynards.com>

---
+++ output.tf
@@ -0,0 +1,29 @@
+### [BEGIN] output.tf ###
+output "id" {
+  description = <<EOD
+  Resource Group id
+  EOD
+  value       = join("", azurerm_resource_group.this.*.id)
+}
+
+output "name" {
+  description = <<EOD
+  Resource Group name
+  EOD
+  value       = join("", azurerm_resource_group.this.*.name)
+}
+
+output "location" {
+  description = <<EOD
+  Resource Group location
+  EOD
+  value       = join("", azurerm_resource_group.this.*.location)
+}
+
+output "tags" {
+  description = <<EOD
+  Resource Group tags
+  EOD
+  value       = merge(azurerm_resource_group.this.*.tags)
+}
+### [END] output.tf ###

1c25dec60d47dc26914159dcc6a7782cef915648
RalphBrynard <ralph@thebrynards.com>
2023-06-21 16:39:38 -0500

    removed var.resource_group_per_resource

--- variables.tf
+++ variables.tf
@@ -1,9 +1,2 @@
 ### [BEGIN] variables.tf ###
-variable "resource_group_per_resource" {
-  description = <<EOD
-  [Optional] If set to `true` a resource group for each resource that will be created with this module.
-  EOD
-  type        = bool
-  default     = false
-}
 ### [END] variables.tf ###

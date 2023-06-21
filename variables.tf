d0b7c652ec765a58b9e6a0f20b990902381593da
RalphBrynard <ralph@thebrynards.com>
2023-06-21 16:18:10 -0500

    var.resource_group_per_resource

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 ### [BEGIN] variables.tf ###
-
+variable "resource_group_per_resource" {
+  description = <<EOD
+  [Optional] If set to `true` a resource group for each resource that will be created with this module.
+  EOD
+  type        = bool
+  default     = false
+}
 ### [END] variables.tf ###
